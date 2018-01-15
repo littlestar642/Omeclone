5f35a1c744fafd0cc8e017ee51806edd77635349
littlestar642 <avinashjaiswal642@gmail.com>
2018-01-15 12:02:34 +0530

    update mock chat page

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ app.set('view engine', 'hbs');
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.get('/chat',(req,res)=>{
+  res.render('chat');
+});
 
 app.listen(8000);
 console.log(`Server is runnning at ${port}`);
